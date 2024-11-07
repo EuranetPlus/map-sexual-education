cddaad45aede0b9bdc67debb86453b27c3c7a8ef
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2024-11-07 09:39:36 +0100

    Update MapChoropleth.svelte

--- src/lib/components/MapChoropleth.svelte
+++ src/lib/components/MapChoropleth.svelte
@@ -410,15 +410,17 @@
 				{#each tooltip as tip}
 					{#if config.datasetType == 'values'}
 						<div class="values">
-							{#if config.datasetUnit == 'percent'}
-								{#if config.percentRounded == true}
-									<span class="font-bold">{formatInt($MOUSE.tooltip.value * 100)}%</span>
-								{:else if config.percentRounded == false}
-									<span class="font-bold">{Math.round($MOUSE.tooltip.value * 1000) / 10}%</span>
-								{/if}
-							{:else if config.datasetUnit == 'fullNumbers'}
-								<span class="font-bold">{$MOUSE.tooltip.value}</span>
-							{/if}
+							{/*
+				{#if config.datasetUnit == 'percent'}
+					{#if config.percentRounded == true}
+						<span class="font-bold">{formatInt($MOUSE.tooltip.value * 100)}%</span>
+					{:else if config.percentRounded == false}
+						<span class="font-bold">{Math.round($MOUSE.tooltip.value * 1000) / 10}%</span>
+					{/if}
+				{:else if config.datasetUnit == 'fullNumbers'}
+					<span class="font-bold">{$MOUSE.tooltip.value}</span>
+				{/if}
+				*/}
 							<span>{tip.label}</span>
 						</div>
 					{/if}
